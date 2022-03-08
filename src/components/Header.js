2e802c9cab01fe7bf6ab583c02c61b4dfb5fbc66
Darshan <darshanrajpurohit47@gmail.com>
2022-03-08 20:24:39 +0530

    my changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -176,9 +176,9 @@ const NavMenu = styled.div`
     }
 }
 
-    // @media (max-width: 768px){
-    //     display:none;
-    // }
+    @media (max-width: 768px){
+        display:none;
+    }
 `
 
 const Login = styled.a`
